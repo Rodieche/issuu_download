5f191a9fafc8e5c8596d256c6bdc8e5aeaeed648
Rodolfo Echenique <echeniquer@gmail.com>
2016-03-14 00:08:21 -0300

    Se logra renombrar los archivos menores a 10 y se crear el cbz...por mejorar : como descargar desde el link de vista de issuu

--- issuu.sh
+++ issuu.sh
@@ -8,8 +8,14 @@ function download {
 	read code
 	i=1
 	while [ $i -le $num ]; do
-		c="http://image.issuu.com/${code}/jpg/page_${i}.jpg"
-		wget $c
+		if [ $i -lt 10 ]; then
+			c="http://image.issuu.com/${code}/jpg/page_${i}.jpg"
+			wget $c
+			mv "page_${i}.jpg" "page_0${i}.jpg"
+		else
+			c="http://image.issuu.com/${code}/jpg/page_${i}.jpg"
+			wget $c
+		fi
 		let i=i+1
 	done
 }
@@ -23,7 +29,17 @@ function move {
 	echo "Archivos movidos exitosamente"
 }
 
-echo "Ingrese el nombre del comic: "
+function make_cbz {
+
+	zip "${1}.cbz" *.jpg
+	rm *.jpg
+}
+
+echo "Ingrese el nombre del comic (sin espacios, use guiones bajos en todo caso): "
 read name
 download
 move $name
+cd $name
+make_cbz $name
+cd ..
+
