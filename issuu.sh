1aeb279c82439d5509b8af996e743697b3c3325d
Rodolfo Echenique <echeniquer@gmail.com>
2016-03-13 22:45:46 -0300

    Se modifica para requisito de codigo

--- issuu.sh
+++ issuu.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 echo "Cuantas paginas contiene el comic? : "
 read num
+echo "Ahora ingrese el codigo del URL : "
+read code
+
+#160309141815-9126d0ee4936850f0f70c6535ce4b705
+
 i=1
 while [ $i -lt $num ]; do
-	c="http://image.issuu.com/150506211233-c003f4cb0e726263c7d6363460158bf6/jpg/page_${i}.jpg"
+	c="https://image.issuu.com/${code}/jpg/page_${i}.jpg"
 	wget $c
 	let i=i+1
 done
